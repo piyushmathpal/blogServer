a1b781c8f4117b2774b44a77b8734a5f09424df4
Piyush Mathpal <mathpal37@gmail.com>
2023-05-25 17:16:37 +0530

    cors2

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin","https://vocal-treacle-8a2496.netlify.app")
+  res.header("Access-Control-Allow-Origin","*")
   res.header("Access-Control-Allow-Methods","GET,OPTIONS,PATCH,DELETE,POST,PUT")
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization,Set-Cookie")
   next()
